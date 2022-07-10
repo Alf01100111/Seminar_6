f3da6a8c73ea0d3a2293d663d65920d09461f3cc
Андрей хухарев <alf.master.mail@gmail.com>
2022-07-10 20:27:57 +0300

    задача 41

---
+++ task 41/Program.cs
@@ -0,0 +1,17 @@
+﻿
+System.Console.WriteLine("Введите кол. вводимых чисел");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int count = 0;
+while (m > 0)
+{
+    System.Console.WriteLine("Введите число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num > 0)
+    {
+        count++;
+    }
+    m--;
+}
+
+System.Console.WriteLine($"Вы ввели {count} чисел больше ноля ");
